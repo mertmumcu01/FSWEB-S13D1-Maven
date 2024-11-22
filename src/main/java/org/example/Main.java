84cb175ae4cb7edc31a3b902f2dc823dd3382b69
mertmumcu01 <merttmumcu@gmail.com>
2024-11-22 10:14:38 +0300

    Java basic practice

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         System.out.println(shouldWakeUp(false,2));
         System.out.println(hasTeen(25,45,20));
         System.out.println(isCatPlaying(true,38));
